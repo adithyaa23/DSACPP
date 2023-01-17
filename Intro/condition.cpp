84c69ad8a6f8c0f79ddfd58de0a753fdf748e594
adithyaa23 <adithya2396@gmail.com>
2023-01-17 16:06:38 +0530

    Conditions examples

--- Intro/condition.cpp
+++ Intro/condition.cpp
@@ -2,13 +2,110 @@
 using namespace std;
 
 int main(){
-    int a = 5;
-    int b;
-    cin>> b;
-    if(a<b){
-        cout<< "a is less than b"<< endl;
-    }else{
-        cout<< "a is greater than b"<< endl;
-    }
+    // int a = 2;
+    // if(a==5){
+    //     cout<< "a is less than b"<< endl;
+    // }if(a>0){
+    //     cout<< "Positive"<<endl;
+    // }
+    // else{
+    //     cout<< "a is greater than b"<< endl;
+    // }
 
-}
+    //guess the output*****
+
+    // int b=a+1;
+    // cout<<"value of a after addition "<<a<<endl;
+    // cout<<"value of b "<<b<<endl;
+    // if((a=3)==b){
+    //     cout<<a;
+    // }else{
+    //     cout<<"adding one"<<a+1;
+    // }
+
+/*~~~~~~~~~~~~~~~~~~~~~ASCII Homework~~~~~~~~~~~~~~*/
+    // int asciVal = cin.get();
+    // if(asciVal >= 65 && asciVal <=90){
+    //     cout<<"Uppercase"<<endl;
+    // }else if(asciVal >= 97 && asciVal <=122){
+    //      cout<<"Lowercase"<<endl;
+    // }else if(asciVal >= 48 && asciVal <=57){
+    // cout<<"Number"<<endl;
+    // }else{
+    //     cout<<"Invalid input"<<endl;
+    // }
+
+    // int n,i=1;
+    // cin>>n;
+    // cout<<i<<endl;
+    // while(i<=n){
+    //     cout<<""<<i;
+    //     i++;
+    // }
+
+    // int n,sum=0,i=1;
+    // cin>>n;
+    // while(i<=n){
+    //     sum = sum +i;
+    //     i++;
+    // }
+    //  cout<<sum;
+    // int n,sum=0,i=2;
+    // cin>>n;
+    // while (i<=n)
+    // {       
+    //     sum=sum+i;
+    //     i=i+2;
+    // }
+    // cout<<sum<<endl;
+    // int n,i=2;
+    // cin>>n;
+    // while(i<n && n%i==0){
+    //     cout<<" "<<n;
+    //     i=i+1;
+    // }
+
+//         int n;
+//         cin>>n;
+//         int i=0;
+//         while(i<n){
+//             int j=0;
+//             while(j<n){
+//                 cout<<"*";
+//                 j++;
+//             }
+//             cout<<"\n";
+//             i++;
+//         }
+// ****
+// ****
+// ****
+// ****
+// int n,i=1;
+// cin>>n;
+// while(i<=n){
+//     int j=1;
+//     while(j<=n){
+//         cout<<i;
+//         j++;
+//     }
+//     cout<<"\n";
+//     i++;
+// }
+// 11111
+// 22222
+// 33333
+// int n,i=1;
+// cin>>n;
+// while(i<=n){
+//     int j=1;
+//     while(j<=n){
+//         cout<<j;
+//         j++;
+//     }
+//     cout<<"\n";
+//     i++;
+// }
+// 123
+// 123
+// 123
